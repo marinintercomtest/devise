f4278c576c3694ab9186063cc1593667b6ed5514
Patrick O'Doherty <p@trickod.com>
2014-03-14 15:42:24 -0700

    Yield the resource being registered to block if passed in
    Taken from
    https://github.com/plataformatec/devise/commit/989071144e80cea01be2ac357730ce98260353bc

--- app/controllers/devise/registrations_controller.rb
+++ app/controllers/devise/registrations_controller.rb
@@ -13,6 +13,7 @@ class Devise::RegistrationsController < DeviseController
     build_resource(sign_up_params)
 
     if resource.save
+      yield resource if block_given?
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_navigational_format?
         sign_up(resource_name, resource)
