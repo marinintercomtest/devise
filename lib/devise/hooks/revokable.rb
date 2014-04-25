531e7dd2ce0c5f76d434f98e29853b99a7e80714
Declan McGrath <declanmg@gmail.com>
2014-04-25 20:08:50 +0100

    WIP

--- lib/devise/hooks/revokable.rb
+++ lib/devise/hooks/revokable.rb
@@ -3,10 +3,7 @@ Warden::Manager.after_set_user :except => :fetch do |record, warden, options|
   env = warden.request.env
 
   if record && record.respond_to?(:revoked?) && options[:store] != false && !env['devise.skip_revokable']
-    revokable_token = SecureRandom.hex
-    record.active_sessions << revokable_token unless record.active_sessions.include? revokable_token
-    record.active_sessions.last(record.max_concurrent_sessions)
-    record.save
+    revokable_token = record.activate_session
     warden.cookies.signed["revokable_token"] = revokable_token
   end
 end
@@ -17,8 +14,7 @@ Warden::Manager.after_fetch do |record, warden, options|
 
   if record && record.respond_to?(:revoked?) && options[:store] != false && !env['devise.skip_revokable']
     if warden.authenticated?(scope)
-      revokable_token = warden.cookies.signed["revokable_token"]
-      if record.revoked?(revokable_token)
+      if record.revoked?(warden.cookies.signed["revokable_token"])
         warden.logout(scope)
         throw :warden, :scope => scope, :message => :unauthenticated
       end
@@ -30,9 +26,7 @@ Warden::Manager.before_logout do |record, warden, options|
   env = warden.request.env
 
   if record && record.respond_to?(:revoked?) && options[:store] != false && !env['devise.skip_revokable`']
-    revokable_token = warden.cookies.signed["revokable_token"]
-    record.active_sessions.delete(revokable_token)
-    record.save
+    record.deactivate_session(warden.cookies.signed["revokable_token"])
     warden.cookies.delete("revokable_token")
   end
 end
--- lib/devise/models/revokable.rb
+++ lib/devise/models/revokable.rb
@@ -9,9 +9,21 @@ module Devise
         ['active_sessions']
       end
 
+      def activate_session
+        revokable_token = SecureRandom.hex
+        record.active_sessions << revokable_token unless record.active_sessions.include? revokable_token
+        record.active_sessions.last(record.max_concurrent_sessions)
+        record.save
+        revokable_token
+      end
+
+      def deactivate_session
+        record.active_sessions.delete(revokable_token)
+        record.save
+      end
+
       def revoked?(revokable_token)
-        false
-        #!active_sessions.include?(revokable_token)
+        !active_sessions.include?(revokable_token)
       end
 
       def max_concurrent_sessions
