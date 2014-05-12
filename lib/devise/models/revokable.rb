612e1cb0244cdc1ba4feb7105f01612db8d93c72
Declan McGrath <declanmg@gmail.com>
2014-05-12 11:34:51 +0100

    Prefix calls to active_sessions with "self." so that active_sessions...
    never treated like a local variable.

--- lib/devise/models/revokable.rb
+++ lib/devise/models/revokable.rb
@@ -11,19 +11,19 @@ module Devise
 
       def activate_revokable_session
         revokable_token = SecureRandom.hex
-        active_sessions << revokable_token unless active_sessions.include? revokable_token
-        active_sessions = active_sessions.last(max_concurrent_sessions)
+        self.active_sessions << revokable_token unless self.active_sessions.include? revokable_token
+        self.active_sessions = self.active_sessions.last(max_concurrent_sessions)
         save
         revokable_token
       end
 
       def deactivate_revokable_session(revokable_token)
-        active_sessions.delete(revokable_token)
+        self.active_sessions.delete(revokable_token)
         save
       end
 
       def revoked?(revokable_token)
-        !active_sessions.include?(revokable_token)
+        !self.active_sessions.include?(revokable_token)
       end
 
       def max_concurrent_sessions
